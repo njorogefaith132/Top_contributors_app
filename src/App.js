3672f54414bef490c4b05d6e0728228423183668
Njorogefaith132 <wairimunjoroge132@gmail.com>
2021-09-08 11:12:26 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import {useEffect, useState}  from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [user, setUser] = useState('');
+  const [repo, setRepo] = useState('');
+  const [contributors, setContributors] = useState([]);
+
+  useEffect(()=>{
+      fetch("https://api.github.com/repos/njorogefaith132/Calculator/contributors?q=contributions&order=desc")
+      .then(res=> res.json() )
+      .then(data => {
+         console.log(data)
+      })
+  
+  }, [])
+
+  const repoChange =(e) =>{
+    setRepo(e.target.value);
+  }
+
+  const userChange = e =>{
+    setUser(e.target.value)
+  }
+  
+  const onrepoSubmit = (e) =>{
+    e.preventDefault();
+    fetch(`https://api.github.com/repos/${user}/${repo}/contributors?q=contributions&order=desc`)
+    .then(res=> res.json() )
+    .then(data => {
+      setContributors(data);
+    }).catch(error =>{
+      console.log(error)
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+    <form onSubmit={onrepoSubmit}>
+         <input type="text" className="inputs" onChange={userChange} />
+         <input type="text" className="inputs" onChange={repoChange} />
+         <button className="btn btn-warning">Submit</button>
+    </form>
+    <div className="user-list">
+      <h3>List of Contributors</h3>
+<div className="lists">
+
+    {
+      contributors.map((contributor, index) =>{
+        return(
+          <div key={index} className="list" >
+            
+              <p>{contributor.login}</p>
+
+           
+
+          </div>
+        )
+      })
+    }
+</div>
+    </div>
     </div>
   );
 }
